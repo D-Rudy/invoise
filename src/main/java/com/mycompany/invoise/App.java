38d8f217b5a212cb5a7365cea8076e41508823fb
D-Rudy <duriez.ry@gmail.com>
2023-02-15 16:48:19 +0100

    Lesson #1 => entity, service, repository

--- src/main/java/com/mycompany/invoise/App.java
+++ src/main/java/com/mycompany/invoise/App.java
@@ -1,5 +1,10 @@
 package com.mycompany.invoise;
 
+import com.mycompany.invoise.entity.Invoice;
+import com.mycompany.invoise.service.InvoiceService;
+
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
@@ -8,6 +13,13 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "What is the customer name?" );
+        Scanner sc = new Scanner(System.in);
+        String customerName = sc.nextLine();
+        Invoice invoice = new Invoice();
+        invoice.setCustomerName(customerName);
+        InvoiceService invoiceService = new InvoiceService();
+        invoiceService.createInvoice(invoice);
+
     }
 }
