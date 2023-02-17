8741780a35168c0836035895930949896c18d387
D-Rudy <duriez.ry@gmail.com>
2023-02-17 18:06:29 +0100

    Lesson #6 => Property

--- src/main/java/com/mycompany/invoise/service/InvoiceServicePrefix.java
+++ src/main/java/com/mycompany/invoise/service/InvoiceServicePrefix.java
@@ -3,9 +3,36 @@ package com.mycompany.invoise.service;
 import com.mycompany.invoise.entity.Invoice;
 import com.mycompany.invoise.repository.InvoiceRepositoryInterface;
 
+import java.io.File;
+
 public class InvoiceServicePrefix implements InvoiceServiceInterface {
-    private static long lastNumber = 112L;
+    private long lastNumber = 112L;
+    private String prefix;
+    private File ficher;
     private InvoiceRepositoryInterface invoiceRepositoryMichel;
+    public File getFicher() {
+        return ficher;
+    }
+
+    public void setFicher(File ficher) {
+        this.ficher = ficher;
+    }
+
+    public long getLastNumber() {
+        return lastNumber;
+    }
+
+    public void setLastNumber(long lastNumber) {
+        this.lastNumber = lastNumber;
+    }
+
+    public String getPrefix() {
+        return prefix;
+    }
+
+    public void setPrefix(String prefix) {
+        this.prefix = prefix;
+    }
 
     public void setInvoiceRepository(InvoiceRepositoryInterface invoiceRepositoryMichel) {
         this.invoiceRepositoryMichel = invoiceRepositoryMichel;
@@ -14,7 +41,7 @@ public class InvoiceServicePrefix implements InvoiceServiceInterface {
     @Override
     public void createInvoice(Invoice invoice) {
 
-        invoice.setNumber("INV_" + (++lastNumber));
+        invoice.setNumber(prefix + (++lastNumber));
         invoiceRepositoryMichel.create(invoice);
     }
 }
