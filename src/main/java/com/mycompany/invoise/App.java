690475370820122b2fd51f3b20df2165edac6c6f
D-Rudy <duriez.ry@gmail.com>
2023-02-18 10:33:46 +0100

    Spring to spring boot

--- src/main/java/com/mycompany/invoise/App.java
+++ src/main/java/com/mycompany/invoise/App.java
@@ -1,29 +1,21 @@
 package com.mycompany.invoise;
 
 import com.mycompany.invoise.controller.InvoiceControllerInterface;
-import com.mycompany.invoise.service.InvoiceServiceInterface;
-import com.mycompany.invoise.service.prefix.InvoiceServicePrefix;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.*;
+import org.springframework.context.annotation.PropertySource;
 
 /**
  * Hello world!
  */
-@Configuration
-@PropertySource("classpath:application.properties")
-@ImportResource("classpath:applicationContext.xml")
-@ComponentScan(basePackages = {"com.mycompany.invoise.controller.web", /*"com.mycompany.invoise.service.number", */"com.mycompany.invoise.repository.database"})
+@SpringBootApplication
 public class App {
     public static void main(String[] args) {
 
-        //ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-        ApplicationContext context = new AnnotationConfigApplicationContext(App.class);
+        ApplicationContext context = SpringApplication.run(App.class);
         InvoiceControllerInterface invoiceController = context.getBean(InvoiceControllerInterface.class);
         invoiceController.createInvoice();
 
-    }
-    @Bean
-    public InvoiceServiceInterface configurationInvoiceService(){
-        return new InvoiceServicePrefix();
     }
 }
