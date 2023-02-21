e678fb44d420ac60d39672d0408aad4f5f5b25c5
D-Rudy <duriez.ry@gmail.com>
2023-02-21 08:46:58 +0100

    Spring to spring boot

--- src/main/java/com/mycompany/invoise/App.java
+++ src/main/java/com/mycompany/invoise/App.java
@@ -4,7 +4,6 @@ import com.mycompany.invoise.controller.InvoiceControllerInterface;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.PropertySource;
 
 /**
  * Hello world!
